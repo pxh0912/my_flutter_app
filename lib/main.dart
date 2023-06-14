10e315bbcaa0121ac91dace2587b8f3f080cf1c6
pxh0912 p <20211060126@mail.ynu.edu.cn>
2023-06-14 04:19:20 +0000

    1

---
+++ lib/main.dart
@@ -0,0 +1,279 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+  
+}
+class AnimatedListRoute extends StatefulWidget {
+  const AnimatedListRoute({Key? key}) : super(key: key);
+
+  @override
+  _AnimatedListRouteState createState() => _AnimatedListRouteState();
+}
+
+class _AnimatedListRouteState extends State<AnimatedListRoute> {
+  var data = <String>[];
+  int counter = 5;
+
+  final globalKey = GlobalKey<AnimatedListState>();
+
+  @override
+  void initState() {
+    for (var i = 0; i < counter; i++) {
+      data.add('${i + 1}');
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        AnimatedList(
+          key: globalKey,
+          initialItemCount: data.length,
+          itemBuilder: (
+            BuildContext context,
+            int index,
+            Animation<double> animation,
+          ) {
+            //添加列表项时会执行渐显动画
+            return FadeTransition(
+              opacity: animation,
+              child: buildItem(context, index),
+            );
+          },
+        ),
+        buildAddBtn(),
+      ],
+    );
+  }
+
+  // 创建一个 “+” 按钮，点击后会向列表中插入一项
+  Widget buildAddBtn() {
+    return Positioned(
+      child: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          // 添加一个列表项
+          data.add('${++counter}');
+          // 告诉列表项有新添加的列表项
+          globalKey.currentState!.insertItem(data.length - 1);
+          print('添加 $counter');
+        },
+      ),
+      bottom: 30,
+      left: 0,
+      right: 0,
+    );
+  }
+
+  // 构建列表项
+  Widget buildItem(context, index) {
+    String char = data[index];
+    return ListTile(
+      //数字不会重复，所以作为Key
+      key: ValueKey(char),
+      title: Text(char),
+      trailing: IconButton(
+        icon: Icon(Icons.delete),
+        // 点击时删除
+        onPressed: () => onDelete(context, index),
+      ),
+    );
+  }
+
+  void onDelete(context, index) {
+    // 待实现
+  }
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+
+
+
+
+
+
+
+
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          
+          children: <Widget>[
+            
+            DecoratedBox(
+              decoration: BoxDecoration(
+              gradient: LinearGradient(colors:[Colors.red,Colors.orange.shade700]), //背景渐变
+              borderRadius: BorderRadius.circular(3.0), //3像素圆角
+              boxShadow: const [ //阴影
+                BoxShadow(
+                  color:Colors.black54,
+                  offset: Offset(2.0,2.0),
+                   blurRadius: 4.0
+                )
+              ]
+             ),
+             child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 18.0),
+                child: Text("Login", style: TextStyle(color: Colors.white),),
+             )
+            ),
+           Column(
+  children: <Widget>[
+    //背景为蓝色，则title自动为白色
+    NavBar(color: Color.fromARGB(255, 251, 61, 124), title: "主题"), 
+    //背景为白色，则title自动为黑色
+    NavBar(color: Color.fromARGB(255, 125, 41, 210), title: "color"),
+  ]
+),
+           
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            
+          ],
+        ),
+      ),
+      
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+        
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+      
+    );
+  }
+}
+
+class NavBar extends StatelessWidget {
+  final String title;
+  final Color color; //背景颜色
+
+  NavBar({
+    Key? key,
+    required this.color,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        minHeight: 52,
+        minWidth: double.infinity,
+      ),
+      decoration: BoxDecoration(
+        color: color,
+        boxShadow: [
+          //阴影
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, 3),
+            blurRadius: 3,
+          ),
+        ],
+      ),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          //根据背景色亮度来确定Title颜色
+          color: color.computeLuminance() < 0.5 ? Colors.white : Colors.black,
+        ),
+      ),
+      alignment: Alignment.center,
+    );
+  }
+}
